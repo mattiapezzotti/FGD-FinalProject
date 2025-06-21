a374559efe5a073aa97f6c2c56a9387ff64872ee
mattiapezzotti <matpezzo01@gmail.com>
2025-06-21 16:36:22 +0200

    migliorato il fuori, aggiunta acqua

---
+++ Assets/Boat.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Boat : MonoBehaviour, IInteractable
+{
+    private Outline outline;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        outline = GetComponent<Outline>();
+        outline.enabled = false;
+    }
+
+    public void Interact()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void DrawOutline(bool b)
+    {
+        outline.enabled = b;
+    }
+}
